5e26f3b5379c663af6442c2b37e548d76de812e0
Erik Hartley <erik>
2018-02-10 18:32:24 -0700

    Initial Commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Erik HARTLEY on 2/10/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C
+//
+//  Created by Erik HARTLEY on 2/10/18.
+//  Copyright © 2018 Erik HARTLEY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
